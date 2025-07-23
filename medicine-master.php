47f7b9fca11def75a10b3e4135a8b7083b2eb906
Yash <yash.g@smytten.com>
2025-07-23 21:51:25 +0530

    changes

--- medicine-master.php
+++ medicine-master.php
@@ -27,6 +27,7 @@
     $token_id    = $csrf->get_token_id();
     
     $token_value = $csrf->get_token($token_id);
+
     $allPatient  = $admin-> getAllPatient();
 
     $results     = $admin->query("SELECT * FROM hp_medicine_master WHERE deleted_time = 0 ORDER BY id DESC");
